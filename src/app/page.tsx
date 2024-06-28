c885480aef36b0e4357e21b9e73ac21f7e572a7a
Lachicagladiadora <200379@unsaac.edu.pe>
2024-06-27 23:20:02 -0500

    feat: add script generator into api and fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,26 @@
 "use client";
+
 import { useState } from "react";
 
 export default function Home() {
-  const [searchUrl, setSearchUrl] = useState<
-    string | number | readonly string[] | undefined
-  >("");
-
+  const [searchUrl, setSearchUrl] = useState("");
   const [template, setTemplate] = useState("");
 
+  const getData = async () => {
+    const data = await fetch("http://localhost:3000/api/hello", {
+      method: "GET",
+    });
+    console.log({ data });
+    const response = await data.json();
+    console.log({ response });
+    const res = response.message;
+    console.log({ res });
+    setTemplate(res);
+  };
+  getData();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-evenly p-24">
+    <main className="flex min-h-screen flex-col items-center justify-evenly p-242345678">
       <form action="" className="flex gap-4 items-center justify-center">
         <input
           type="url"
@@ -17,14 +28,15 @@ export default function Home() {
           id=""
           value={searchUrl}
           onChange={(e) => setSearchUrl(e.target.value)}
+          className="text-black"
         />
         <button className="hover:border hover:border-violet-400 p-2 rounded-lg">
           Search
         </button>
       </form>
-      <div className="border border-red-500 rounded-md p-8 text-rose-200">
+      <pre className="min-w-[300px] min-h-[400px] border border-red-500 rounded-md p-8 text-rose-200">
         {template}
-      </div>
+      </pre>
     </main>
   );
 }
---
+++ src/pages/api/hello.ts
@@ -0,0 +1,154 @@
+"use sever"
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+import puppeteer, { Page } from "puppeteer";
+import fs from "fs";
+
+const YOUTUBE_SEARCH_URL = "https://www.youtube.com/results";
+const ALBUM_URL =
+  "https://musicbrainz.org/release/de2c9c1d-62e6-40f3-b55e-56389992777a"; // 👈 fill this
+
+type AlbumData = {
+  albumTitle: string;
+  albumCover: string;
+  albumYear: string;
+  songsName: string[];
+};
+
+const wait = (time: number = 300) =>
+  new Promise((res) => {
+    const timerId = setTimeout(() => {
+      clearTimeout(timerId);
+      res(true);
+    }, time);
+  });
+
+const getUrl = (baseUrl: string, query?: string) => {
+  const url = new URL(baseUrl);
+  if (query) {
+    const params = new URLSearchParams();
+    params.append("search_query", query);
+    url.search = params.toString();
+  }
+  return url.toString();
+};
+
+const getAlbumData = async (page: Page) => {
+  return await page.evaluate(() => {
+    const songsName = (
+      Array.from(document.querySelectorAll("tbody tr td.title")) as HTMLElement[]
+    ).map((c) => c.innerText.split("\n")[0].trim());
+  
+    const albumTitle = document.querySelector("h1")?.innerText;
+    if (!albumTitle) throw Error("Error: Can't detect album title");
+
+    const albumCover = (
+      document.querySelector(".artwork-image") as HTMLAnchorElement
+    )?.href;
+    const albumYear = (document.querySelector(".release-date") as HTMLElement)
+      ?.innerText;
+
+    const albumData: AlbumData = {
+      albumTitle,
+      albumCover,
+      albumYear,
+      songsName,
+    };
+    console.log({ albumData });
+    return albumData;
+  });
+};
+
+const getSongLink = async (page: Page) => {
+  return await page.evaluate(() => {
+    const videoTitle = document.querySelector(
+      "a#video-title"
+    ) as HTMLAnchorElement;
+
+    if (!videoTitle) return "";
+    return videoTitle.href;
+  });
+};
+
+const buildTemplate = (
+  albumTitle: string,
+  albumCover: string,
+  albumYear: string,
+  songs: { title: string; youtubeId: string }[]
+) => {
+  const albumCoverData = albumCover ? `thumbnail="${albumCover}"` : "\r";
+  const albumYearData = albumYear ? `year="${albumYear}"` : "\r";
+  const data = songs.reduce(
+    (acu, cur) => `${acu}\t\t\t${JSON.stringify(cur)},\n`,
+    ""
+  );
+  const songsData = `[\n${data}\t\t]`;
+  return `
+<AlbumMusic
+    title="${albumTitle}"
+    ${albumCoverData}
+    ${albumYearData}
+    songs={${songsData}}
+/>
+`;
+};
+
+const getQueryParameter = (url: string, queryParameter: string): string => {
+  const urlObj = new URL(url);
+  const params = urlObj.searchParams;
+  return params.get(queryParameter) || "";
+};
+
+const getAlbum = async (albumUrl:string) => {
+  if (!ALBUM_URL) throw Error("Error: Album url is empty!");
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1080, height: 1024 });
+
+  await page.goto(ALBUM_URL);
+  console.log("0/n");
+  const { songsName, albumTitle, albumCover, albumYear } = await getAlbumData(
+    page
+  );
+  console.log("1/n");
+
+  const songs = [];
+  for (let i = 0; i < songsName.length; i++) {
+    const c = songsName[i];
+    const searchQuery = `${albumTitle} | ${c}`;
+    const searchUrl = getUrl(YOUTUBE_SEARCH_URL, searchQuery);
+    console.log("11 ->", searchUrl);
+    await page.goto(searchUrl);
+    await wait();
+    console.log("12");
+    const link = await getSongLink(page);
+    console.log("13", link);
+    const videoId = getQueryParameter(link, "v");
+    console.log(`${i + 2}/${songsName.length + 1}`, videoId);
+    songs.push({ title: c, youtubeId: videoId });
+  }
+  const template = buildTemplate(albumTitle, albumCover, albumYear, songs);
+  console.log("✅ done!");
+  console.log({template})
+  fs.writeFileSync("./template.txt", template);
+  await browser.close();
+  return template
+}
+
+type ResponseData = {
+  message: string
+}
+ 
+export default async function handler(
+  req: string,
+  res: NextApiResponse<ResponseData>
+) {
+try {
+  const albumData = await getAlbum(req)
+  res.status(200).json({ message: albumData })
+  return albumData
+} catch (error) {
+  res.status(500).json({message: 'an error ocurred, try again later'})
+}
+}
